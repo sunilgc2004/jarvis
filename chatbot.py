71b752ec85f9523f43878f220da9bd3f90b07019
SNEHA G C <sunilgc4454@gmail.com>
2026-05-23 21:25:31 +0530

    Updated Jarvis

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="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"
-)
-
-def ask_ai(question):
-
+    api_key=" API KEY "
     response = client.chat.completions.create(
         model="gpt-4.1-mini",
         messages=[
